4fc8d6b4d9a2c0843a3494f662a68045e977c8d8
Connor Taffe <cpaynetaffe@gmail.com>
2015-09-19 18:49:24 -0500

    identifies keywords properly.

--- main.c
+++ main.c
@@ -14,12 +14,13 @@
 #include <stdbool.h>
 #include <string.h>
 #include <assert.h>
-#include <ctype.h>
 
 #include "hash.c"
 
-// Secion 3, Program Form
-// Categories dealing with characters:
+/*
+ * Secion 3, Program Form
+ * Categories dealing with characters
+ */
 enum {
 	CHAR_DIGITS,      // Section 3.1.1
 	CHAR_LETTERS,     // Section 3.1.2
@@ -33,30 +34,120 @@ char *chars[] = {
 	[CHAR_SPECIAL] = " =+-*/(),.$"
 };
 
+bool charIn(char c, int i) {
+	char *digits = chars[i];
+	while (*digits) {
+		if (c == *digits) {
+			return true;
+		}
+		digits++;
+	}
+	return false;
+}
+
+bool charDigit(char c) {
+	return charIn(c, CHAR_DIGITS);
+}
+
+bool charLetter(char c) {
+	return charIn(c, CHAR_LETTERS);
+}
+
+bool charAlphanumeric(char c) {
+	return charIn(c, CHAR_DIGITS) | charIn(c, CHAR_LETTERS);
+}
+
+bool charSpecial(char c) {
+	return charIn(c, CHAR_SPECIAL);
+}
+
 enum {
-	KEYWORD_END
+	// Keywords
+	_KEYWORD_BEGIN,
+	KEYWORD_END,
+	KEYWORD_GOTO,
+	KEYWORD_IF,
+	KEYWORD_CALL,
+	KEYWORD_RETURN,
+	KEYWORD_CONTINUE,
+	KEYWORD_DO,
+	_KEYWORD_END,
+
+	// Operators
+	_KEYWORD_OP_BEGIN,
+
+	// Arithmetic Expression Operators (Section 6.1)
+	_KEYWORD_OP_ARITHMETIC_BEGIN,
+	KEYWORD_OP_ADD,
+	KEYWORD_OP_SUB,
+	KEYWORD_OP_MUL,
+	KEYWORD_OP_DIV,
+	KEYWORD_OP_EXP,
+	_KEYWORD_OP_ARITHMETIC_END,
+
+	// Relational Expression Operators (Section 6.2)
+	_KEYWORD_OP_RELATIONAL_BEGIN,
+	KEYWORD_OP_LT,
+	KEYWORD_OP_LE,
+	KEYWORD_OP_EQ,
+	KEYWORD_OP_NE,
+	KEYWORD_OP_GT,
+	KEYWORD_OP_GE,
+	_KEYWORD_OP_RELATIONAL_END,
+
+	// Logical Expression Operators (Section 6.3)
+	_KEYWORD_OP_LOGICAL_BEGIN,
+	KEYWORD_OP_OR,
+	KEYWORD_OP_AND,
+	KEYWORD_OP_NOT,
+	_KEYWORD_OP_LOGICAL_END,
+
+	_KEYWORD_OP_END,
 };
 
 char *keywords[] = {
-	[KEYWORD_END] = "END"
+	[KEYWORD_END] = "END",
+	[KEYWORD_GOTO] = "GOTO",
+	[KEYWORD_IF] = "IF",
+	[KEYWORD_CALL] = "CALL",
+	[KEYWORD_RETURN] = "RETURN",
+	[KEYWORD_CONTINUE] = "CONTINUE",
+	[KEYWORD_DO] = "DO",
+
+	// Arithmetic
+	[KEYWORD_OP_ADD] = "+",
+	[KEYWORD_OP_SUB] = "-",
+	[KEYWORD_OP_MUL] = "*",
+	[KEYWORD_OP_DIV] = "/",
+	[KEYWORD_OP_EXP] = "**",
+
+	// Relational
+	[KEYWORD_OP_LT] = ".LT.",
+	[KEYWORD_OP_LE] = ".LE.",
+	[KEYWORD_OP_EQ] = ".EQ.",
+	[KEYWORD_OP_NE] = ".NE.",
+	[KEYWORD_OP_GT] = ".GT.",
+	[KEYWORD_OP_GE] = ".GE.",
+
+	// Logical
+	[KEYWORD_OP_OR] = ".OR.",
+	[KEYWORD_OP_AND] = ".AND.",
+	[KEYWORD_OP_NOT] = ".NOT."
 };
 
 enum {
-	MAX_LINE_LENGTH = 72
+	MAX_LINE_LENGTH = 72,
+	MAX_SYMBOLIC_NAME_LENGTH = 6,
+	PREFIX_LENGTH = 6
 };
-
-typedef struct {
-	FILE *input;
-	char *buf;
-	int i, sz;
-	int col, line;
-} Lexer;
 
 enum {
 	LEXICAL_TOKEN_ERROR,
 	LEXICAL_TOKEN_COMMENT,
 	LEXICAL_TOKEN_END,
-	LEXICAL_TOKEN_LABEL
+	LEXICAL_TOKEN_LABEL,
+	LEXICAL_TOKEN_IDENT,
+	LEXICAL_TOKEN_KEYWORD,
 };
 
 struct LexicalToken {
@@ -64,6 +155,16 @@ struct LexicalToken {
 	char *str;
 	int col, line;
 };
+
+typedef struct {
+	FILE *input;
+	char *buf;
+	int i, sz;
+	int col, line;
+
+	struct LexicalToken *toks;
+	int toki, ntoks;
+} Lexer;
 
 int lexerNext(Lexer *l) {
 	if (l->i == l->sz) {
@@ -86,48 +187,63 @@ int lexerPeek(Lexer *l) {
 	return c;
 }
 
-struct LexicalToken lexerEmit(Lexer *l) {
+struct LexicalToken *lexerEmit(Lexer *l, int type) {
+	// Construct token
 	struct LexicalToken t = {
 		.str = l->buf,
+		.type = type,
 		.col = l->col,
 		.line = l->line
 	};
 	t.str[l->i] = '\0'; // null terminate
+
+	// Reset lexer
 	l->i = 0;
 	l->sz = 5;
 	l->buf = calloc(sizeof(char), l->sz);
-	return t;
+
+	// Append to toks queue
+	if (l->toki >= l->ntoks) {
+		l->ntoks = (l->ntoks+1)*2;
+		l->toks = calloc(sizeof(struct LexicalToken), l->ntoks);
+	}
+	l->toks[l->toki] = t;
+	return &l->toks[l->toki++];
 }
 
 void lexicalTokenPprint(struct LexicalToken *tok) {
-	printf("%d:%d '%s'\n", tok->line+1, tok->col+1, tok->str);
+	printf("%d:%d (%d) '%s'\n", tok->line+1, tok->col+1, tok->type, tok->str);
 }
 
-void error(Lexer *l, char *msg) {
-	struct LexicalToken t = lexerEmit(l);
-	t.type = LEXICAL_TOKEN_ERROR;
-	free(t.str);
-	t.str = msg;
-	lexicalTokenPprint(&t);
+void error(Lexer *l, char *msg, char *sec) {
+	struct LexicalToken *t = lexerEmit(l, LEXICAL_TOKEN_ERROR);
+	free(t->str);
+	if (sec != NULL) {
+		asprintf(&t->str, "Error: %s. (ASA FORTRAN S. %s)", msg, sec);
+	} else {
+		asprintf(&t->str, "Error: %s.", msg);
+	}
 }
 
 // Error: Exceeds maximum line length
 void errorMaxLineLengthExceeded(Lexer *l, const char *line_type) {
 	char *str;
-	asprintf(&str, "error: %s exceeds maximum line length (%d).", line_type, MAX_LINE_LENGTH);
-	error(l, str);
+	asprintf(&str, "%s exceeds maximum line length (%d)", line_type, MAX_LINE_LENGTH);
+	error(l, str, "3.2");
 	free(str);
 }
 
 // Error: Early program termination
 void errorEarlyProgramTermination(Lexer *l, const char *line_type) {
 	char *str;
-	asprintf(&str, "error: %s terminated program, expected end line.", line_type);
-	error(l, str);
+	asprintf(&str, "%s terminated program, expected End Line.", line_type);
+	error(l, str, "3.2.2");
 	free(str);
 }
 
-// Section 3.2 Lines
+/*
+ * Section 3.2 Lines
+ */
 enum {
 	LINE_ERROR,
 	LINE_COMMENT,        // Section 3.2.1
@@ -136,18 +252,16 @@ enum {
 	LINE_CONTINUATION    // Section 3.2.4
 };
 
-enum {
-	PREFIX_LENGTH = 6
-};
-
 void *stateStart(Lexer *l);
 
 void *stateCommentLine(Lexer *l) {
 	int c;
-	// Read through the rest of the line;
-	// comment terminates on \n.
-	// Watch for early program terminations (EOF).
-	// Error on maximum line length exceedance.
+	/*
+	 * Read through the rest of the line;
+	 * comment terminates on \n.
+	 * Watch for early program terminations (EOF).
+	 * Error on maximum line length exceedance.
+	 */
 	while((c = lexerNext(l)) != EOF && c != '\n' && l->col < MAX_LINE_LENGTH) {}
 	if (l->col == MAX_LINE_LENGTH) {
 		// Error: Exceeds maximum line length
@@ -159,103 +273,147 @@ void *stateCommentLine(Lexer *l) {
 		return NULL;
 	} else {
 		// Emit comment line.
-		struct LexicalToken *t = calloc(sizeof(struct LexicalToken), 1);
-		*t = lexerEmit(l);
-		lexicalTokenPprint(t);
-		free(t->str);
-		free(t);
+		lexerEmit(l, LEXICAL_TOKEN_COMMENT);
 		return stateStart;
 	}
 }
 
-void *stateEndLine(Lexer *l) {
-	int c;
-	char *keyword = keywords[KEYWORD_END];
-	while ((c = lexerNext(l)) != EOF && c != '\n' && l->col < MAX_LINE_LENGTH) {
-		if (*keyword && c == *keyword) {
-			keyword++;
+void *stateLine(Lexer *l);
+
+/*
+ * Symbolic Names (Section 3.5, 10.1-10.1.10)
+ *
+ * Maximum of MAX_SYMBOLIC_NAME_LENGTH characters long.
+ */
+void *stateIdent(Lexer *l) {
+	// One character already lexed.
+	int c, seen = 0;
+	char ident[7] = {};
+	while ((c = lexerPeek(l)) != EOF
+		&& c != '\n'
+		&& l->col < MAX_LINE_LENGTH) {
+		if (charLetter(c)) {
+			// Record identifier
+			ident[seen] = c;
+			seen++;
+			if (seen > MAX_SYMBOLIC_NAME_LENGTH) {
+				char *str;
+				asprintf(&str, "Identifier exceeds maximum Symbolic Name length (%d)", MAX_SYMBOLIC_NAME_LENGTH);
+				error(l, str, "3.5");
+				free(str);
+				return NULL;
+			}
 		} else if (c != ' ') {
-			// unexpected character in end line
-			char *str;
-			asprintf(&str, "error: Unexpected character in end line, expected '%c'.", *keyword);
-			error(l, str);
-			free(str);
-			return NULL;
+			// Junk characters, signals end of number, break.
+			break;
 		}
+		lexerNext(l);
 	}
-	if (l->col == MAX_LINE_LENGTH) {
-		// Error: Line exceeds maximum length
-		errorMaxLineLengthExceeded(l, "end line");
-	} else if (*keyword) {
-		// Error: Didn't scan entire keyword
-		char *str;
-		asprintf(&str, "error: Expected keyword '%s', missing.", keyword);
-		error(l, str);
-		free(str);
+	if (c == EOF) {
+		errorEarlyProgramTermination(l, "Symbolic Name");
 		return NULL;
+	} else {
+		for (int i = _KEYWORD_BEGIN+1; i < _KEYWORD_END; i++) {
+			if (strcmp(ident, keywords[i]) == 0) {
+				lexerEmit(l, LEXICAL_TOKEN_KEYWORD);
+				return NULL;
+			}
+		}
+		// Lexed up to non-digit.
+		lexerEmit(l, LEXICAL_TOKEN_IDENT);
+		return stateLine;
 	}
-	struct LexicalToken *t = calloc(sizeof(struct LexicalToken), 1);
-	*t = lexerEmit(l);
-	t->type = LEXICAL_TOKEN_END;
-	lexicalTokenPprint(t);
-	free(t->str);
-	free(t);
 	return NULL;
 }
 
+/*
+ * Combined End (Section 3.2.2), Initial (Section 3.2.3),
+ * and Continuation (Section 3.2.4) lines.
+ *
+ * End and Continuation lines cannot have labels (handled later).
+ */
+void *stateLine(Lexer *l) {
+	int c;
+	while ((c = lexerPeek(l)) != EOF
+		&& c != '\n'
+		&& l->col < MAX_LINE_LENGTH) {
+		// Lex line and stuff
+		if (charLetter(c)) {
+			return stateIdent;
+		} else {
+			error(l, "Unknown Character in Line", "3.1");
+			return NULL;
+		}
+		lexerNext(l);
+	}
+	if (l->col == MAX_LINE_LENGTH) {
+		// Error: Line exceeds maximum length
+		errorMaxLineLengthExceeded(l, "End Line");
+	}
+	return NULL;
+}
+
+/*
+ * Statement Label (Section 3.4)
+ *
+ * Optionally prefixes a line, but not an End Line
+ * or Continuation Line.
+ *
+ * Used as reference points for GOTO, DO, etc.
+ */
 void *statePrefixLabel(Lexer *l) {
 	int c;
 	while ((c = lexerNext(l)) != EOF
 		&& c != '\n'
-		&& ((l->col < 5 && isdigit(c))
+		&& ((l->col < 5 && charDigit(c))
 			// column 5, '0' or ' ' only
 			|| (l->col == 5 && c == '0')
 			|| c == ' ')
 		&& l->col < PREFIX_LENGTH) {}
 	if (c == EOF) {
-		errorEarlyProgramTermination(l, "prefix label");
+		errorEarlyProgramTermination(l, "Label in Line Prefix");
 		return NULL;
 	} else if (c == '\n') {
-		error(l, "error: Line terminated before prefix ended.");
+		error(l, "Line terminated before Line Prefix ended", "3.2.2");
 		return NULL;
 	} else if (l->col != PREFIX_LENGTH) {
-		error(l, "error: Malformed label.");
+		error(l, "Malformed Label", "3.4");
 		return NULL;
 	}
 	// Emit label
-	struct LexicalToken *t = calloc(sizeof(struct LexicalToken), 1);
-	*t = lexerEmit(l);
-	t->type = LEXICAL_TOKEN_LABEL;
-	lexicalTokenPprint(t);
-	free(t->str);
-	free(t);
+	lexerEmit(l, LEXICAL_TOKEN_LABEL);
 	// Only Initial lines can have labels.
-	return NULL;
+	return stateLine;
 }
 
+/*
+ * Lines (Section 3.2)
+ * Columns 1-6 of a FORTRAN Line.
+ */
 void *statePrefix(Lexer *l) {
 	int c = lexerNext(l);
 	if (c == 'C') {
 		// Comment
 		return stateCommentLine;
 	}
-	do {
+	while (c != EOF && c != '\n' && l->col < PREFIX_LENGTH) {
 		// Label or solid blanks
-		if (isdigit(c)) {
+		if (charDigit(c)) {
 			// Found a Label
 			return statePrefixLabel;
 		} else if (c == ' ') {
 			// Blank, ignored
 		} else {
 			char *str;
-			asprintf(&str, "error: Unexpected character '%c' in prefix.", c);
-			error(l, str);
+			asprintf(&str, "Unexpected character '%c' in Line Prefix", c);
+			error(l, str, "3.2");
 			free(str);
 			return NULL;
 		}
-	} while((c = lexerNext(l)) != EOF && c != '\n' && l->col < PREFIX_LENGTH);
+		c = lexerNext(l);
+	}
 	if (c == EOF) {
-		errorEarlyProgramTermination(l, "line prefix");
+		errorEarlyProgramTermination(l, "Line Prefix");
 		return NULL;
 	} else if (c == '\n') {
 		if (l->i == 1) {
@@ -263,12 +421,12 @@ void *statePrefix(Lexer *l) {
 			l->i = 0;
 			return stateStart;
 		} else {
-			error(l, "error: Truncated prefix.");
+			error(l, "Truncated Line Prefix", "3.2");
 			return NULL;
 		}
 	} else {
 		// Initial or End line.
-		return NULL;
+		return stateLine;
 	}
 }
 
@@ -291,6 +449,10 @@ int main() {
 
 	while (func != NULL) {
 		func = ((void*(*)(Lexer*)) func)(&l);
+		while (l.toki) {
+			l.toki--;
+			lexicalTokenPprint(&l.toks[l.toki]);
+		}
 	}
 	free(l.buf);
 	free(h.buckets);
